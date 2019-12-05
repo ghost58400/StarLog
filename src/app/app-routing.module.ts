d03fc34b145dd2f98a4fe9a6c68a2515716364ba
Joffrey LAURENT <jojo58400@hotmail.fr>
2019-12-05 21:12:58 +0100

    Ajout page 404 (composant error)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {ErrorComponent} from './error/error.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '404', component: ErrorComponent },
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
