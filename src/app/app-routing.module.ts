9a24d0bba0ebba44b7b5d981035cafcaac3d561c
Logan Coquerant <locoqu@ttu.ee>
2019-12-05 21:26:09 +0100

    merge + route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import {ErrorComponent} from './error/error.component';
+import { ErrorComponent } from './error/error.component';
+import { SignupComponent } from "./signup/signup.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '404', component: ErrorComponent },
   {path: '**', redirectTo: '/404'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
     HomeComponent,
     ErrorComponent
   ],
