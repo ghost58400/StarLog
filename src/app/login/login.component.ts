38855815d2107d1b75c100447009261fcd915510
olivier <olivierdacosta7@gmail.com>
2019-12-06 01:46:19 +0100

    ça va tout casser ?

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  submit() {
+    console.log(this.form.value);
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
