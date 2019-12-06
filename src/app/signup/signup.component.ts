a90b76034b27cf6e044602262a11fb63377cec2d
Logan Coquerant <locoqu@ttu.ee>
2019-12-06 05:40:02 +0100

    je crois que c'est bon pour les fichiers

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 import {ApiService} from "../api.service";
 import {Router} from "@angular/router";
+import { UploadService } from  '../upload.service';
 
 
 @Component({
@@ -13,8 +14,11 @@ import {Router} from "@angular/router";
 export class SignupComponent {
 
   form: FormGroup;
+  error: string;
+  userId: number = 1;
+  uploadResponse = { status: '', message: '', filePath: '' };
 
-  constructor(private formBuilder: FormBuilder, private api: ApiService,private router: Router,) {
+  constructor(private formBuilder: FormBuilder, private api: ApiService,private router: Router, private uploadService: UploadService) {
     // create our form group with all the inputs we will be using in the template
 
     this.form = this.formBuilder.group({
@@ -26,15 +30,31 @@ export class SignupComponent {
       codepostal: ['', Validators.required],
       description: ['', Validators.required],
       jaide: [false],
-      aled: [false]
+      aled: [false],
+      pdp: ['']
     });
+  }
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.form.get('pdp').setValue(file);
+    }
   }
 
   submit(form: NgForm) {
     if (this.form.valid) {
+      const formData = new FormData();
+      formData.append('file', this.form.get('pdp').value);
+
+      this.uploadService.upload(formData, this.userId).subscribe(
+        (res) => this.uploadResponse = res,
+        (err) => this.error = err
+      );
+      console.log(this.form.value);
       this.api.signUp(form).subscribe(
         res => {
-          this.router.navigate(['/matcher']);
+          // this.router.navigate(['/matcher']);
         },
         err => {
           console.log(err);
